f68df3aa4882dc16537705f9cfaa67592050af45
HelloZOOO <qnrwnd10119@gmail.com>
2023-02-23 11:43:10 +0900

    오전커밋 - 보스 예측방향 구현

--- Quad Action/Assets/Script/Boss.cs
+++ Quad Action/Assets/Script/Boss.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class Boss : Enemy
 {
-    // Start is called before the first frame update
+    public GameObject missile;
+    public Transform missilePortA;
+    public Transform missilePortB;
+
+    //플레이어의 이동방향을 예측하는 변수
+    Vector3 lookVec;
+    Vector3 tauntVec;
+    bool isLook;
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if(isLook)
+        {
+            float h = Input.GetAxisRaw("Horizontal");
+            float v = Input.GetAxisRaw("Vertical");
+            //얼마나 예측할것인가? (5) 만큼 예측하겠다
+            lookVec = new Vector3(h,0,v) * 5f;
+            transform.LookAt(target.position + lookVec);
+        }
     }
 }
