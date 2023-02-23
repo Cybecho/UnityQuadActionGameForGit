22a13df6f2e6e6d1438797cb9ddc7d1248abd0c4
HelloZOOO <qnrwnd10119@gmail.com>
2023-02-23 14:30:28 +0900

    보스 패턴 랜덤 분기점 생성

--- Quad Action/Assets/Script/Boss.cs
+++ Quad Action/Assets/Script/Boss.cs
@@ -13,9 +13,9 @@ public class Boss : Enemy
     Vector3 tauntVec;
     bool isLook;
 
-    void Start()
+    void Awake()
     {
-        
+        StartCoroutine(Think());
     }
 
     void Update()
@@ -27,6 +27,28 @@ public class Boss : Enemy
             //얼마나 예측할것인가? (5) 만큼 예측하겠다
             lookVec = new Vector3(h,0,v) * 5f;
             transform.LookAt(target.position + lookVec);
+        }
+    }
+
+    IEnumerator Think()
+    {
+        yield return new WaitForSeconds(0.1f); //생각하는 시간 길수록 보스가 쉬워진다
+        
+        //랜덤으로 0~4값이 랜덤액션값에 들어간다
+        //보스가 랜덤값에 따라 다른 패턴을 가지기 위함이다
+        int ranAction = Random.Range(0,5);
+        switch (ranAction)
+        {
+            case 0:
+                break;
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            case 4:
+                break;
         }
     }
 }
