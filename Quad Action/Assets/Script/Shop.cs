dbe85ae08cfe6e4a12368666c7d5f383fa3afe7d
HelloZOOO <qnrwnd10119@gmail.com>
2023-02-25 23:22:44 +0900

    UI - 상점창 구현

---
+++ Quad Action/Assets/Script/Shop.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shop : MonoBehaviour
+{
+    public RectTransform uiGroup; //UI를 담을 변수
+    public Animator anim;
+    Player enterPlayer; //플레이어 정보를 상점이 받는 변수
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
